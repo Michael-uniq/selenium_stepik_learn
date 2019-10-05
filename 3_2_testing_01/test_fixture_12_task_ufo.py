d84297d5f05cbda944b8699643e9fc714a9d19d9
Michael-gitbash <michael.co@ya.ru>
2019-10-05 18:06:05 +0300

    fix name conftest.py and rewrite task ufo

--- 3_2_testing_01/test_fixture_12_task_ufo.py
+++ 3_2_testing_01/test_fixture_12_task_ufo.py
@@ -5,33 +5,24 @@ import time
 import math
 
 
-@pytest.fixture(scope='module')
-def browser():
-    print("\nstart browser for test..")
-    dr = webdriver.Chrome()
-    yield dr
-    print("\nquit browser..")
-    dr.quit()
-
-
 def answer():
     return str(math.log(int(time.time())))
 
 urls = [
-    'https://stepik.org/lesson/236895/step/1',
-    'https://stepik.org/lesson/236896/step/1',
-    'https://stepik.org/lesson/236897/step/1',
-    'https://stepik.org/lesson/236898/step/1',
-    'https://stepik.org/lesson/236899/step/1',
-    'https://stepik.org/lesson/236903/step/1',
-    'https://stepik.org/lesson/236904/step/1',
-    'https://stepik.org/lesson/236905/step/1',
+    '236895',
+    '236896',
+    '236897',
+    '236898',
+    '236899',
+    '236903',
+    '236904',
+    '236905',
 ]
 
 
 @pytest.mark.parametrize("urls", urls)
 def test_correct_or_message(browser: webdriver.Chrome, urls):
-    browser.get(urls)
+    browser.get(f'https://stepik.org/lesson/{urls}/step/1')
     browser.implicitly_wait(10)
     browser.find_element(By.CLASS_NAME, "textarea").send_keys(answer())
     browser.find_element(By.CLASS_NAME, "submit-submission").click()
